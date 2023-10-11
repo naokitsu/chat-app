0a142bc0e7655ae15b8597af58487c9cdf502a3f
naokitsu <naokitsu@tutanota.com>
2023-10-11 19:41:32 +0300

    User registration

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,31 +1,68 @@
 use openssl::hash::{Hasher, MessageDigest};
 use openssl::rand::rand_bytes;
 use rocket::http::Status;
-use rocket::serde::json::serde_json::{json, Value};
+use rocket::serde::json::serde_json::{self, json, Value};
 use rocket::serde::{json, Deserialize, Serialize};
+use rocket::State;
+use sqlx::{Pool, Sqlite, SqlitePool};
+use std::io::ErrorKind::AlreadyExists;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
+#[serde(crate = "rocket::serde")]
 pub struct UserRegistration {
     username: String,
     password: String,
 }
 
-#[post("/users", data = "<user>")]
-pub async fn register_user(user: json::Json<UserRegistration>) -> Status {
-    let hashed_password = match hash_password(user.password.as_str()) {
-        Ok(x) => x,
-        _ => return Status::InternalServerError,
-    };
-
-    println!(
-        "New user registered: {:?}",
-        (&user.username, &hashed_password)
-    );
-
-    Status::Created
+#[derive(Debug, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Token {
+    token: String,
 }
 
-fn hash_password(password: &str) -> Result<Box<[u8; 48]>, openssl::error::ErrorStack> {
+#[post("/users", data = "<user>")]
+pub async fn register_user(
+    pool: &State<Pool<Sqlite>>,
+    user: json::Json<UserRegistration>,
+) -> Result<json::Json<Token>, Status> {
+    let mut connection = pool
+        .acquire()
+        .await
+        .map_err(|_| Status::InternalServerError)?;
+
+    let query_result = sqlx::query!(
+        "SELECT COUNT(*) as count FROM users WHERE username = ?",
+        user.username
+    )
+    .fetch_one(&mut *connection)
+    .await
+    .map_err(|_| Status::InternalServerError)?;
+
+    if query_result.count > 0 {
+        return Err(Status::BadRequest);
+    }
+
+    let hashed_password =
+        hash_password(user.password.as_str()).map_err(|_| Status::InternalServerError)?;
+
+    let token = Token::generate_token().map_err(|_| Status::InternalServerError)?;
+
+    let insert_result = sqlx::query!(
+        "INSERT INTO users (username, salted_hash) VALUES (?, ?)",
+        user.username,
+        hashed_password
+    )
+    .execute(&mut *connection)
+    .await
+    .map_err(|e| {
+        eprintln!("Error inserting user into the database: {:?}", e);
+        Status::InternalServerError
+    })?;
+
+    Ok(json::Json(token))
+}
+
+fn hash_password(password: &str) -> Result<Vec<u8>, openssl::error::ErrorStack> {
     let mut salt = [0u8; 16];
     rand_bytes(&mut salt).unwrap();
 
@@ -38,5 +75,18 @@ fn hash_password(password: &str) -> Result<Box<[u8; 48]>, openssl::error::ErrorS
     result[..16].copy_from_slice(&salt[..]);
     result[16..].copy_from_slice(&hash[..]);
 
-    Ok(result)
+    Ok(result.to_vec())
+}
+
+impl Token {
+    fn generate_token() -> Result<Token, openssl::error::ErrorStack> {
+        let mut random_bytes = [0u8; 32];
+        rand_bytes(&mut random_bytes)?;
+        let token = random_bytes
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .collect::<String>();
+
+        Ok(Token { token })
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ mod auth;
 #[macro_use]
 extern crate rocket;
 
+use dotenvy::dotenv;
 use rocket::http::Status;
 use rocket::serde::json::serde_json::{json, Value};
 use rocket::serde::{json, Deserialize, Serialize};
+use sqlx::SqlitePool;
+use std::env;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct User {
@@ -14,6 +17,21 @@ struct User {
 }
 
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![auth::register_user])
+async fn rocket() -> _ {
+    dotenv().ok();
+    let database_url =
+        env::var("DATABASE_URL").expect("Couldn't get the database URL from the environment");
+
+    let pool = SqlitePool::connect(database_url.as_str())
+        .await
+        .expect("Couldn't connect to the database");
+
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .expect("Couldn't migrate the database tables");
+
+    rocket::build()
+        .mount("/", routes![auth::register_user])
+        .manage(pool)
 }
