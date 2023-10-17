0f8c52060de42bdb5d1c09aaf4ecaf5175a21273
naokitsu <naokitsu@tutanota.com>
2023-10-17 18:26:27 +0300

    reformat snowflakes

--- src/snowflake/error.rs
+++ src/snowflake/error.rs
@@ -2,27 +2,27 @@ use std::num::TryFromIntError;
 use std::sync::PoisonError;
 use std::time::SystemTimeError;
 
-pub(super) enum SnowflakeError {
+pub(super) enum Error {
     TimestampOverflow,
     SequenceOverflow,
     SystemTime,
     PoisonError,
 }
 
-impl From<SystemTimeError> for SnowflakeError {
+impl From<SystemTimeError> for Error {
     fn from(_value: SystemTimeError) -> Self {
-        SnowflakeError::SystemTime
+        Error::SystemTime
     }
 }
 
-impl From<TryFromIntError> for SnowflakeError {
+impl From<TryFromIntError> for Error {
     fn from(value: TryFromIntError) -> Self {
-        SnowflakeError::TimestampOverflow
+        Error::TimestampOverflow
     }
 }
 
-impl From<PoisonError<_>> for SnowflakeError {
+impl From<PoisonError<_>> for Error {
     fn from(value: TryFromIntError) -> Self {
-        SnowflakeError::PoisonError
+        Error::PoisonError
     }
 }
--- src/snowflake/mod.rs
+++ src/snowflake/mod.rs
@@ -1,31 +1,8 @@
-use std::num::TryFromIntError;
-use std::sync::{Arc, Mutex, PoisonError};
-use std::time::{SystemTime, SystemTimeError};
+mod error;
 
-enum SnowflakeError {
-    TimestampOverflow,
-    SequenceOverflow,
-    SystemTime,
-    PoisonError,
-}
-
-impl From<SystemTimeError> for SnowflakeError {
-    fn from(_value: SystemTimeError) -> Self {
-        SnowflakeError::SystemTime
-    }
-}
-
-impl From<TryFromIntError> for SnowflakeError {
-    fn from(value: TryFromIntError) -> Self {
-        SnowflakeError::TimestampOverflow
-    }
-}
-
-impl From<PoisonError<_>> for SnowflakeError {
-    fn from(value: TryFromIntError) -> Self {
-        SnowflakeError::PoisonError
-    }
-}
+use self::error::Error;
+use std::sync::{Arc, Mutex};
+use std::time::SystemTime;
 
 struct SnowflakeGenerator {
     epoch: SystemTime,
@@ -35,13 +12,13 @@ struct SnowflakeGenerator {
 }
 
 impl SnowflakeGenerator {
-    fn next(&mut self) -> Result<u64, SnowflakeError> {
+    fn next(&mut self) -> Result<u64, Error> {
         let mut time = self.get_local_time()?;
         let mut prev_time = self.prev_time.lock()?;
         if *prev_time == time {
             self.sequence += 1;
             if self.sequence > 0b111_111_111_111 {
-                return Err(SnowflakeError::SequenceOverflow);
+                return Err(Error::SequenceOverflow);
             }
         } else {
             self.sequence = 0;
@@ -55,13 +32,13 @@ impl SnowflakeGenerator {
         Ok(time << 22 | self.id << 12 | self.sequence)
     }
 
-    fn get_local_time(&self) -> Result<u64, SnowflakeError> {
+    fn get_local_time(&self) -> Result<u64, Error> {
         let now = SystemTime::now().duration_since(self.epoch)?;
         let a = u64::try_from(now.as_millis());
 
         match u64::try_from(now.as_millis()) {
             Ok(x) if x < 2u64.pow(41) => Ok(x),
-            _ => Err(SnowflakeError::TimestampOverflow),
+            _ => Err(Error::TimestampOverflow),
         }
     }
 }
